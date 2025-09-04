278ccbbe356f77115adf29d7190ebcda10403f0b
GG <ggpro7w7@gmail.com>
2025-09-04 07:33:26 -0600

    Parcial01 - Add CORS policy to API

--- API/Program.cs
+++ API/Program.cs
@@ -11,10 +11,19 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 //app.UseAuthorization();
+
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins(
+        "http://localhost:4200",
+        "https://localhost:4200"
+    ));
 
 app.MapControllers();
 
