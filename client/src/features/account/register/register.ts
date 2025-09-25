2f35a04878cac119a5f2e17a70f1c8eec3954db4
GG <ggpro7w7@gmail.com>
2025-09-25 07:33:48 -0600

    Parcial02 - Add Register form

---
+++ client/src/features/account/register/register.ts
@@ -0,0 +1,23 @@
+import { Component, input } from '@angular/core';
+import { User } from '../../../types/user';
+import { RegisterCreds } from '../../../types/registerCreds';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css'
+})
+export class Register {
+  membersFromHome = input.required<User[]>();
+  protected creds = {} as RegisterCreds;
+
+  register(): void {
+    console.log(this.creds);
+  }
+
+  cancel(): void {
+    console.log('Cancel executed');
+  }
+}
